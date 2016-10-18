79ef4eade43c09c3e208e771d95ee8b419d92d84
Olenka <olenka_zhdan@ukr.net>
2016-10-18 09:34:54 +0000

    add new file

---
+++ images.js
@@ -0,0 +1,5 @@
+// Open a URL in a lightbox
+var lightbox = lity('//images/nails/nails1.jpg);
+
+// Bind as an event handler
+$(document).on('click', '[data-lightbox]', lity);
--- javascript/form.js
+++ javascript/form.js
@@ -10,7 +10,7 @@ $(function () {
             },
             dataType: "json"
         }).done(function() {
-            $('form').html('<h1>Thank you!</h1>')
+            $('form').html('<h1>Дякуюємо!</h1>')
         }).fail(function(xhr, err) {
             $('form').html(xhr.statusText);
         });        
